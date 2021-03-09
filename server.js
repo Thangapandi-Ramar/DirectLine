b570134f5048213a3e366e74a6a0bd051ee136f1
ThangapandiRamar <56972351+ThangapandiRamar@users.noreply.github.com>
2021-03-09 14:35:26 +0000

    updated

--- server.js
+++ server.js
@@ -9,11 +9,11 @@ const express = require('express');
 const app = express();
 app.use(requireHTTPS);
 
-app.use(express.static('./dist/direct-line'));
+app.use(express.static('./dist/DirectLine'));
 
 
 app.get('/*', function(req, res) {
-  res.sendFile('index.html', {root: 'dist/direct-line/'}
+  res.sendFile('index.html', {root: 'dist/DirectLine/'}
 );
 });
 
