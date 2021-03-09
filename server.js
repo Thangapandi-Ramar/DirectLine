322254003f6bc12c0e717171e787bf4bf056edc0
ThangapandiRamar <56972351+ThangapandiRamar@users.noreply.github.com>
2021-03-09 14:18:59 +0000

    updated

--- server.js
+++ server.js
@@ -1,32 +1,20 @@
-// server.js
-const express = require('express');
-const app = express();
-// Run the app by serving the static files
-// in the dist directory
-app.use(express.static(__dirname + '/dist'));
-// Start the app by listening on the default
-// Heroku port
-app.listen(process.env.PORT || 8080);
-
-/ If an incoming request uses
-// a protocol other than HTTPS,
-// redirect that request to the
-// same url but with HTTPS
-const forceSSL = function() {
-  return function (req, res, next) {
-    if (req.headers['x-forwarded-proto'] !== 'https') {
-      return res.redirect(
-       ['https://', req.get('Host'), req.url].join('')
-      );
+function requireHTTPS(req, res, next) {
+    // The 'x-forwarded-proto' check is for Heroku
+    if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
+        return res.redirect('https://' + req.get('host') + req.url);
     }
     next();
-  }
 }
-// Instruct the app
-// to use the forceSSL
-// middleware
-app.use(forceSSL());
+const express = require('express');
+const app = express();
+app.use(requireHTTPS);
+
+app.use(express.static('./dist/direct-line'));
+
 
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
+  res.sendFile('index.html', {root: 'dist/direct-line/'}
+);
 });
+
+app.listen(process.env.PORT || 8080);
